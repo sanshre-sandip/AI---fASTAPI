cb9e98895978fac693441cd2db7ec78fccb5e753
Sanshre-Sandip <sandpipe98@gmail.com>
2026-06-08 16:24:38 +0545

    >>>>>>>

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Path, Query
 import json
 
 app = FastAPI()
@@ -15,22 +15,50 @@ def show():
     
     return data
 
-
+#path parameter
 @app.get('/path/{id}')
-def path_pram(id: str):
+def path_pram(id: str = Path(..., description='id no for client accoding to stored in dataset', examples='1')):
     with open('data.json','r') as f:
         data = json.load(f)
 
         if id in data:
             return data[id]
-        return {'error':'data not found'}
+        
+        raise HTTPException(status_code=404, detail='not found')
 
 
 
+#Query parameter
+from fastapi import FastAPI, Query, HTTPException
 
-   
+@app.get('/sort')
+def sort(
+    sort_by: str = Query(..., description='Sort by height, weight, or bmi'),
+    order: str = Query(..., description='asc or des')
+):
 
-      
+    valid = ['height', 'weight', 'bmi']
 
+    if sort_by not in valid:
+        raise HTTPException(
+            status_code=400,
+            detail=f'Invalid field. Choose from {valid}'
+        )
 
+    if order not in ['asc', 'des']:
+        raise HTTPException(
+            status_code=400,
+            detail='Order must be asc or des'
+        )
 
+    data = show()
+
+    reverse = True if order == 'des' else False
+
+    sorted_data = sorted(
+        data,
+        key=lambda x: x.get(sort_by, 0),
+        reverse=reverse
+    )
+
+    return sorted_data
