26d8ffff4272db36e17f406d33fe265a18a0fec5
Sanshre-Sandip <sandpipe98@gmail.com>
2026-06-08 14:11:52 +0545

    -----

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
 @app.get("/")
 def letsgoo():
     return {'message':'FAstapi for ml'}
+
+
+@app.get("/data")
+def show():
+    with open('data.json','r') as f:
+        data = json.load(f)
+    
+    return data
