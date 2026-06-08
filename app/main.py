49dac57f40f12120a73712aba681c64f08314670
Sanshre-Sandip <sandpipe98@gmail.com>
2026-06-08 16:31:18 +0545

    ignore pycache files

--- app/main.py
+++ app/main.py
@@ -14,3 +14,23 @@ def show():
         data = json.load(f)
     
     return data
+
+
+@app.get('/path/{id}')
+def path_pram(id: str):
+    with open('data.json','r') as f:
+        data = json.load(f)
+
+        if id in data:
+            return data[id]
+        return {'error':'data not found'}
+
+
+
+
+   
+
+      
+
+
+
