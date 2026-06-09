2d5717160deb6c368a5852ca051aff0fe1b9497e
Sanshre-Sandip <sandpipe98@gmail.com>
2026-06-09 10:06:40 +0545

    py

--- app/run.py
+++ app/run.py
@@ -1,9 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, AnyUrl, Field                             
+
+#pydantic provides the custom datatype like EmailStr AnyUrl
 from typing import List,TypedDict, Optional, Dict
 class Student(BaseModel):
     name: str
-    age: int
+    age: int = Field(gt=0)
     relation: Optional[Dict[str, str]] = None
+    email: Optional[EmailStr] = None
+    url: Optional[AnyUrl] = None
 
 
 
@@ -19,17 +23,24 @@ def update_data(student: Student):
     print(student.name)
     print(student.age)
     print(student.relation)
+    print(student.email)
+    print(student.url)
     print('done') 
 
 student_info = {
     "name": "Sandip",
-    "age": 30,
+    "age": 1,
     
 }
 
 patient1 = Student(**student_info)
 update_data(patient1)
 
-insert_data(patient1)
+#insert_data(patient1)
+
+
+#pydantic provides the custom datatype like EmailStr AnyUrl
+
+
 
 
